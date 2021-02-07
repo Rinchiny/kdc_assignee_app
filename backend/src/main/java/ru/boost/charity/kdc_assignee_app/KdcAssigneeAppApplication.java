d0449ab4e353b2a5abf6261608cd604b7f434423
Rinchin Iakovlev <rbyakovlev@alfabank.ru>
2021-02-07 16:26:12 +0500

    Initial commit

---
+++ backend/src/main/java/ru/boost/charity/kdc_assignee_app/KdcAssigneeAppApplication.java
@@ -0,0 +1,13 @@
+package ru.boost.charity.kdc_assignee_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdcAssigneeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdcAssigneeAppApplication.class, args);
+    }
+
+}
